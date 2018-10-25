eb38c25685d4058c538798d1ed75f15216c18543
JacquesDelfrate <jacques.delfrate@gmail.com>
2018-10-26 00:03:20 +0200

    tokenization script load text and clean it

---
+++ tokenizationText.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 25 23:51:03 2018
+
+@author: jacques
+"""
+
+def clean_text(string_value):
+    clean_value = string_value.replace(',', ' ')
+    return(clean_value)
